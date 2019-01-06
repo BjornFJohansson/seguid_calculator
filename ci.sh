386a1aba02f396c31f69340519aba055d3f3635d
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-01-06 12:44:37 +0000

    restart appveyor build

--- ci.sh
+++ ci.sh
@@ -1,23 +1,66 @@
 #!/usr/bin/env bash
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh --quiet
-bash ~/miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-conda update --yes -q conda
-#conda create -yq -n pydnacondabuild35 python=3.5
-#source activate pydnacondabuild35
-conda install -c gsecars wxpython
-pip install https://github.com/pyinstaller/pyinstaller/archive/develop.zip
-brew install upx
-#conda install --yes -c conda-forge pyinstaller
-conda install -c conda-forge pypandoc
-python setup.py build
-cat build/lib/seguid_calculator/_version.py
-pyinstaller --onefile --windowed seguid_calculator.spec
-cat build/seguid_calculator/warnseguid_calculator.txt
-hdiutil create dist/seguid_calculator.dmg -srcfolder dist/ -ov
-zip -r dist/seguid_calculator.zip dist/seguid_calculator.app
-ls dist/
-wc -c dist/seguid_calculator.zip
-wc -c dist/seguid_calculator.dmg
-pip install -I dropbox
-python send_to_dropbox.py
+echo "=============================================================="
+echo "BASH_VERSION" $BASH_VERSION
+echo $(git --version)
+tagname="$(git describe --abbrev=0 --tags)"
+tag="$(git rev-list $tagname | head -n 1)"
+com="$(git rev-parse HEAD)"
+dirty=$(git describe --tags --dirty --always)
+msg=$(git log -1 --pretty=%B)
+echo "=============================================================="
+echo "Establish git variables:"
+echo "=============================================================="
+echo "Current commit hash : $com"
+echo "Dirty tag           : $dirty"
+echo "Commit msg          : $msg"
+echo "=============================================================="
+echo "Environment variables:"
+echo "=============================================================="
+echo "CI                   = $CI"
+echo "APPVEYOR             = $APPVEYOR"
+echo "CIRCLECI             = $CIRCLECI"
+echo "TRAVIS               = $TRAVIS"
+echo "CODESHIP             = $CI_NAME"
+echo "=============================================================="
+echo "Build information:"
+echo "=============================================================="
+if [[ "$com" = "$tag" ]]&&[[ $dirty = $tagname ]]
+then
+    echo "Tagged commit      : $tagname"
+    tagged_commit=true
+    re_final="^[0-9]\.[0-9]\.[0-9]$"
+    re_pre="^[0-9]\.[0-9]\.[0-9](a|b|rc)[0-9]+$"
+    if [[ $tagname =~  $re_final ]]
+    then
+        echo "Tag indicate Final release"
+        echo "deploy to pypi and anaconda.org with label 'main'."
+        pypiserver="pypi"
+        condalabel="main"
+    elif  [[ $tagname =~ $re_pre ]]
+    then
+        echo "Release tag indicate pre release"
+        echo "deploy sdist zip package to pypi "
+        echo "conda package anaconda.org with label 'test'."
+        pypiserver="testpypi"
+        condalabel="test"
+    else
+        echo "Build cancelled because"
+        echo "release tag ($tagname) was not recognized"
+        echo "or"
+        echo "$dirty != $tagname"
+        exit 0
+    fi
+elif [[ $msg = *"skip"* ]]
+then
+    echo "Not a tagged commit"
+    echo "'skip' found in commit msg: '$msg'"
+    echo "tests and builds skipped."
+    echo "=============================================================="
+    exit 0 
+else
+    echo "'skip' not found in commit msg: '$msg'"
+    echo "but commit not tagged or tag dirty"
+    echo "test suit will be run."
+    tagged_commit=false
+
+unset VIRTUAL_ENV
