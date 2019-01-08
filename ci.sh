1df08d5de5efb73c789ee8b247727c27babb2dfe
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-01-08 12:12:12 +0000

    added icon

--- ci.sh
+++ ci.sh
@@ -6,6 +6,6 @@ conda config --prepend channels conda-forge
 conda config --append channels BjornFJohansson
 conda create -yq -n seguidcondabuild36 python=3.6 wxpython pyinstaller
 source activate seguidcondabuild36
-pyinstaller --onefile --windowed seguid_calculator.py
+pyinstaller --onefile --windowed --icon=calc.ico seguid_calculator.py
 wc -c dist/seguid_calculator.exe
 appveyor PushArtifact dist/seguid_calculator.exe
