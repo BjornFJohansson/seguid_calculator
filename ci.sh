fda4da457c170f5560fd5ee2237931f4054fc668
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-01-08 08:31:46 +0000

    fiddle with pyinstaller

--- ci.sh
+++ ci.sh
@@ -8,7 +8,7 @@ conda create -yq -n seguidcondabuild36 python=3.6 wxpython pyinstaller
 source activate seguidcondabuild36
 python setup.py build
 cat build/lib/seguid_calculator/_version.py
-pyinstaller --onefile --noconsole seguid_calculator/seguid.py
+pyinstaller --onefile seguid_calculator/seguid.py
 ls dist/
 wc -c dist/seguid.exe
 appveyor PushArtifact dist/seguid.exe
