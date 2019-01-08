b8bed9423069844646b818a20263311f598b6407
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2019-01-08 11:43:20 +0000

    reduce complexity

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import versioneer
-
-for line in open('seguid_calculator/__init__.py'):
-    if line.startswith('__') and not line.startswith('__version') and not line.startswith('__long'):
+for line in open('seguid_calculator.py'):
+    if line.startswith('__'):
         exec(line.strip())
 
 from setuptools import setup
 
 setup(  name='seguid_calculator',
-        version=versioneer.get_version()[:5],
-        cmdclass=versioneer.get_cmdclass(),
+        version=__version__,
         author          =__author__,
         author_email    =__email__,
-        packages=['seguid_calculator'],
-        entry_points = { 'gui_scripts': [ 'seguid_calculator = seguid_calculator.seguid:main' ]},
+        py_modules=['seguid_calculator'],
+        scripts=['seguid_calculator.py'],
+        entry_points = { 'gui_scripts': [ 'seguid_calculator = seguid_calculator:main' ]},
         url='http://pypi.python.org/pypi/seguid_calculator',
         license='LICENSE.txt',
         description='''Calculates seguid, lseguid & cseguid checksums for biological sequences''',
